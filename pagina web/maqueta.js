457d4113a452af5ab6d0b8e90305375b5d333fe5
Cristian Bournissent <cristianbournissent09@gmail.com>
2023-08-10 10:51:56 -0300

    login

--- pagina web/maqueta.js
+++ pagina web/maqueta.js
@@ -11,32 +11,57 @@ $(document).ready(function(){
   var posts=[
     {
       title:'Prueba de titulo 1',
-      datte: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMMM') +' del ' + moment().format('YYYY'),
+      date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMMM') +' del ' + moment().format('YYYY'),
       conent: 'Por iniciativa de Armando Ittig los primeros colores de la divisa fueron el blanco (camiseta) y el azul (pantalón), pero pronto se abandonaron para adoptar en carácter definitivo el verde y negro a rayas verticales.El Club Social y Deportivo Porvenir nace el 15 de diciembre de 1951. ​Ambos tienen vida propia hasta el 23 de agosto de 1989 en que, fusionados, se denominan con el nombre: Club Social y Deportivo Platense Porvenir. Actualmente cuenta con 11 hectáreas sobre Boulevard Lovatto.'
     },
     {
       title:'Prueba de titulo 2',
-      datte: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMMM') +' del ' + moment().format('YYYY'),
+      date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMMM') +' del ' + moment().format('YYYY'),
       conent: 'Por iniciativa de Armando Ittig los primeros colores de la divisa fueron el blanco (camiseta) y el azul (pantalón), pero pronto se abandonaron para adoptar en carácter definitivo el verde y negro a rayas verticales.El Club Social y Deportivo Porvenir nace el 15 de diciembre de 1951. ​Ambos tienen vida propia hasta el 23 de agosto de 1989 en que, fusionados, se denominan con el nombre: Club Social y Deportivo Platense Porvenir. Actualmente cuenta con 11 hectáreas sobre Boulevard Lovatto.'
     },
     {
       title:'Prueba de titulo 3',
-      datte: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMMM') +' del ' + moment().format('YYYY'),
+      date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMMM') +' del ' + moment().format('YYYY'),
       conent: 'Por iniciativa de Armando Ittig los primeros colores de la divisa fueron el blanco (camiseta) y el azul (pantalón), pero pronto se abandonaron para adoptar en carácter definitivo el verde y negro a rayas verticales.El Club Social y Deportivo Porvenir nace el 15 de diciembre de 1951. ​Ambos tienen vida propia hasta el 23 de agosto de 1989 en que, fusionados, se denominan con el nombre: Club Social y Deportivo Platense Porvenir. Actualmente cuenta con 11 hectáreas sobre Boulevard Lovatto.'
     },
     {
       title:'Prueba de titulo 4',
-      datte: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMMM') +' del ' + moment().format('YYYY'),
+      date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMMM') +' del ' + moment().format('YYYY'),
       conent: 'Por iniciativa de Armando Ittig los primeros colores de la divisa fueron el blanco (camiseta) y el azul (pantalón), pero pronto se abandonaron para adoptar en carácter definitivo el verde y negro a rayas verticales.El Club Social y Deportivo Porvenir nace el 15 de diciembre de 1951. ​Ambos tienen vida propia hasta el 23 de agosto de 1989 en que, fusionados, se denominan con el nombre: Club Social y Deportivo Platense Porvenir. Actualmente cuenta con 11 hectáreas sobre Boulevard Lovatto.'
     },
     {
       title:'Prueba de titulo 5',
-      datte: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMMM') +' del ' + moment().format('YYYY'),
+      date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMMM') +' del ' + moment().format('YYYY'),
       conent: 'Por iniciativa de Armando Ittig los primeros colores de la divisa fueron el blanco (camiseta) y el azul (pantalón), pero pronto se abandonaron para adoptar en carácter definitivo el verde y negro a rayas verticales.El Club Social y Deportivo Porvenir nace el 15 de diciembre de 1951. ​Ambos tienen vida propia hasta el 23 de agosto de 1989 en que, fusionados, se denominan con el nombre: Club Social y Deportivo Platense Porvenir. Actualmente cuenta con 11 hectáreas sobre Boulevard Lovatto.'
     },
 
    
 
   ];
-  console.log(posts);
+ posts.forEach((item, index) => {
+        var post = ` 
+        <article class="post">
+ <h2>${item.title}</h2>
+ <br/>
+ <span class="date"><h3>${item.date}</h3></span>
+ <br/>
+ <p>
+ ${item.conent}
+ </p>
+ <a href="#" class="button-more">Leer mas</a>
+</article>
+        `;
+        
+        $('#posts').append(post);
+ })
+
+//login falso
+
+$('#login form').submit(function(){
+
+
+});
+
+
+
 });
